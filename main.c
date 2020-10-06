606509eef766855530e674b751a115465148931e
dk5ee <ernstpeterboehm@gmail.com>
2020-10-06 06:46:49 +0000

    maybe working

--- main.c
+++ main.c
@@ -7,28 +7,16 @@
 
 #include "servos.h"
 
-void blinki(uint8_t c) {
-	cli();
-	while (1) {
-		for (uint8_t i=0; i<c;i++) {
-			PORTB |= (1<<5);
-			_delay_ms(100);
-			PORTB &= ~(1<<5);
-			_delay_ms(200);
-		}
-		_delay_ms(1000);
-	}
-}
-
 int main(void) {
 	DDRB |= (1<<5); // here is a led
 	servo_enable();
+
 	while (1) {
-		PORTB |= (1<<5);
+		//PORTB |= (1<<5);
 		set_servo1(3500);
 		set_servo2(3500);
 		_delay_ms(500);
-		PORTB &= ~(1<<5);
+		//PORTB &= ~(1<<5);
 		set_servo1(2500);
 		set_servo2(2500);
 		_delay_ms(500);
